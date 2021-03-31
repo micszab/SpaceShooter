f2c635db4c3650772c312ac3fbc8c02f27c2d92b
micszab <micszablewski@gmail.com>
2021-03-31 21:37:30 +0200

    add project files

---
+++ inc/Bullet.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+class Bullet
+{
+private:
+	sf::Sprite shape;
+
+	sf::Vector2f direction;
+	float movementSpeed;
+
+public:
+	Bullet(sf::Texture* texture, float pos_x, float pos_y, float dir_x,  float dir_y, float movement_speed);
+	virtual ~Bullet();
+
+	//Accessor
+	const sf::FloatRect getBounds() const;
+
+	void update();
+	void render(sf::RenderTarget* target);
+};
+
---
+++ inc/Enemy.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class Enemy
+{
+private:
+	unsigned pointCount;
+	sf::CircleShape shape;
+	int type;
+	float speed;
+	int hp;
+	int hpMax;
+	int damage;
+	int points;
+
+	void initShape();
+	void initVariables();
+
+public:
+	Enemy(float pos_x, float pos_y);
+	virtual ~Enemy();
+
+	//Accessors
+	const sf::FloatRect getBounds() const;
+	const int& getPoints() const;
+	const int& getDamage() const;
+
+	//Functions
+	void update();
+	void render(sf::RenderTarget* target);
+};
+
